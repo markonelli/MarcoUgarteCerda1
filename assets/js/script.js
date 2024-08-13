0da66c21902501c03414c32598264e6f9f6a43c3
markonelli <markonelli.u@gmail.com>
2024-08-13 01:07:24 -0400

    ultima actualizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,57 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-items");
+const btnToggleNav = document.querySelector(".menu-btn");
+const workEls = document.querySelectorAll(".work-box");
+const workImgs = document.querySelectorAll(".work-img");
+const mainEl = document.querySelector("main");
+const yearEl = document.querySelector(".footer-text span");
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+const theme = localStorage.getItem("theme") || "dark";
+    document.body.classList.add(theme);
+    
 
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
 
-    if (nombre && email && mensaje) {
-      alert(`¡Gracias, ${nombre}! Tu mensaje ha sido enviado`);
-      form.reset(); // Limpiar formulario
-    } else {
-      alert('Debe completar todos los campos para de enviar');
-    }
-  });
+btnToggleNav.addEventListener("click", toggleNav);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.localName === "a") {
+    toggleNav();
+  }
 });
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+    toggleNav();
+  }
+});
+
+workImgs.forEach((workImg) => workImg.classList.add("transform"));
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    const [textbox, picture] = Array.from(entry.target.children);
+    if (entry.isIntersecting) {
+      picture.classList.remove("transform");
+      Array.from(textbox.children).forEach(
+        (el) => (el.style.animationPlayState = "running")
+      );
+    }
+  },
+  { threshold: 0.3 }
+);
+
+workEls.forEach((workEl) => {
+  observer.observe(workEl);
+});
+
+const lastFocusedEl = document.querySelector('a[data-focused="last-focused"]');
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Tab" && document.activeElement === lastFocusedEl) {
+    e.preventDefault();
+    btnToggleNav.focus();
+  }
+});
+
+yearEl.textContent = new Date().getFullYear();
